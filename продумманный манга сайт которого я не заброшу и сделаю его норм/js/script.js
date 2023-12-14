2eb9c2ede7f7a2692c66face292f0460e58c1a12
geniusss96 <outcast1bro@gmail.com>
2023-12-14 15:12:57 +0500

    sss

---
+++ продумманный манга сайт которого я не заброшу и сделаю его норм/js/script.js
@@ -0,0 +1,8 @@
+let newAlert = "BETA version"
+alert(newAlert)
+console.log(newAlert);
+
+const informationMenu = document.getElementById("o-nas");
+informationMenu.addEventListener("click", () => {
+  alert("BETA version");
+});
