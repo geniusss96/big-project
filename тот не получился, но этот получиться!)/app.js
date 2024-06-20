78d4e7976beb4b129d0e551fe19256bf26446dfd
geniusss96 <outcast1bro@gmail.com>
2024-06-20 09:55:04 +0500

    51

--- тот не получился, но этот получиться!)/app.js
+++
@@ -1,6 +0,0 @@
-// Toggle search input on click
-document.querySelector('.search-button').addEventListener('click', function() {
-  var searchContainer = this.parentElement;
-  var inputField = searchContainer.querySelector('.input');
-  inputField.classList.toggle('active');
-});
